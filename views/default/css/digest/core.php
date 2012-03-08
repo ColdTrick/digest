0ebf8adb80c79f3f217cf22e04ca2c1c35458788
jdalsem <jdalsem@coldtrick.com>
2012-03-08 15:07:36 +0100

    updated css for new digest style

--- views/default/css/digest/core.php
+++ views/default/css/digest/core.php
@@ -5,22 +5,13 @@
 ?>
 
 body {
-	background: #dedede;
+	background: #f6f6f6;
 	color: #333333;
-	font: 80%/1.4 "Lucida Grande",Verdana,sans-serif;
+	font: 80%/1.4 "Lucida Grande",Arial,Tahoma,Verdana,sans-serif;
 }
 
 a {
 	color: #4690d6;
-}
-
-table {
-	width: 100%;
-}
-
-table a img {
- 	float: left;
- 	margin-right: 5px;
 }
 
 img {
@@ -34,49 +25,78 @@ h3 {
 	margin: 0;
 }
 
+#digest_header {
+	background: #4690d6;
+	
+	border-top: 1px solid #dbdbdb;
+	border-left: 1px solid #dbdbdb;
+	border-bottom: 1px solid #dbdbdb;
+	border-right: 1px solid #dbdbdb;
+	
+	-webkit-border-radius: 5px 5px 0 0;
+	-moz-border-radius: 5px 5px 0 0;
+	border-radius: 5px 5px 0 0;
+}
+
+#digest_header {
+	color: #FFFFFF;
+	font-size: 18px;
+	padding: 10px 30px;
+	min-height: 20px;
+}
+
 #digest_container {
 	padding: 20px 0;
 	width: 600px;
 	margin: 0 auto;
 }
 
-#digest_wrapper {
-	background: #FFFFFF;
-	padding: 5px 5px 0 5px;
-}
-
 #digest_content {
 	min-height: 100px;
-	padding:0 20px 20px;
 }
 
-#digest_content h2 {
-	font-size: 16px;
+.elgg-module-digest {
+	background: #FFFFFF;
+	padding: 30px;
+	
+	border-top: 1px solid #FFFFFF;
+	border-left: 1px solid #dbdbdb;
+	border-bottom: 1px solid #dbdbdb;
+	border-right: 1px solid #dbdbdb;
 }
 
-#digest_content h1 a,
-#digest_content h2 a,
-#digest_content h3 a {
+.elgg-module-digest h1 a,
+.elgg-module-digest h2 a,
+.elgg-module-digest h3 a {
 	text-decoration: none;
 }
 
-#digest_content h1 a:hover,
-#digest_content h2 a:hover,
-#digest_content h3 a:hover {
+.elgg-module-digest a:hover,
+.elgg-module-digest a:hover,
+.elgg-module-digest a:hover {
 	text-decoration: underline;
 }
 
-#digest_content h3 {
+.elgg-module-digest h3 {
 	font-size: 14px;
 }
 
-#digest_content .strapline {
-	color: #aaaaaa;
-	line-height: 1em;
+#digest_unsubscribe {
+	font-size: 11px;
+	color: #999999;
+	padding: 20px;
 }
 
 #digest_footer {
-	background: #B6B6B6;
-	font-size: 11px;
-	padding: 20px;
+	padding: 30px;
+	background: #F0F0F0;
+	
+	border-top: 1px solid #FFFFFF;
+	border-left: 1px solid #dbdbdb;
+	border-bottom: 1px solid #dbdbdb;
+	border-right: 1px solid #dbdbdb;
+	
+	-webkit-border-radius: 0 0 5px 5px;
+	-moz-border-radius: 0 0 5px 5px;
+	border-radius: 0 0 5px 5px;
 }
