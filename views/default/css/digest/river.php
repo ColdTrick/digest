0a7da9a47b9eb2d568f8f4ebe00e54a7f80931e6
Jeroen Dalsem <jdalsem@coldtrick.com>
2012-11-26 12:16:36 +0100

    added digest optimized views

--- views/default/css/digest/river.php
+++ views/default/css/digest/river.php
@@ -17,12 +17,6 @@
 	margin-right: 5px;
 }
 
-.elgg-river-item .elgg-avatar > .elgg-menu,
-.elgg-river-responses,
-.elgg-menu-river {
-	display: none;
-}
-
 .elgg-list-river > li {
 	border-bottom: 1px solid #CCC;
 	min-height: 55px;
@@ -53,49 +47,7 @@
 .elgg-river-attachments .elgg-icon {
 	float: left;
 }
-.elgg-river-layout .elgg-input-dropdown {
-	float: right;
-	margin: 10px 0;
-}
 
-.elgg-river-comments-tab {
-	display: block;
-	background-color: #EEE;
-	color: #4690D6;
-	margin-top: 5px;
-	width: auto;
-	float: right;
-	font-size: 85%;
-	padding: 1px 7px;
-	
-	-webkit-border-radius: 5px 5px 0 0;
-	-moz-border-radius: 5px 5px 0 0;
-	border-radius: 5px 5px 0 0;
-}
-
-.elgg-river-comments {
-	margin: 0;
-	border-top: none;
-}
-.elgg-river-comments li:first-child {
-	-webkit-border-radius: 5px 0 0;
-	-moz-border-radius: 5px 0 0;
-	border-radius: 5px 0 0;
-}
-.elgg-river-comments li:last-child {
-	-webkit-border-radius: 0 0 5px 5px;
-	-moz-border-radius-bottomleft: 0 0 5px 5px;
-	border-radius-bottomleft: 0 0 5px 5px;
-}
-.elgg-river-comments li {
-	background-color: #EEE;
-	border-bottom: none;
-	padding: 4px;
-	margin-bottom: 2px;
-}
-.elgg-river-comments .elgg-media {
-	padding: 0;
-}
 .elgg-river-more {
 	background-color: #EEE;
 	
@@ -106,21 +58,4 @@
 	padding: 2px 4px;
 	font-size: 85%;
 	margin-bottom: 2px;
-}
-
-.elgg-river-item form {
-	background-color: #EEE;
-	padding: 4px;
-	
-	-moz-border-radius: 5px;
-	-webkit-border-radius: 5px;
-	border-radius: 5px;
-	
-	height: 30px;
-}
-.elgg-river-item input[type=text] {
-	width: 80%;
-}
-.elgg-river-item input[type=submit] {
-	margin: 0 0 0 10px;
 }
