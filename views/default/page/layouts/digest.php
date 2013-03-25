c25d90a9f935158bcf1093b99306f10e6e27d7d9
Jerome Bakker <jeabakker@coldtrick.com>
2013-03-25 11:21:57 +0100

    fixed: HTML language in Digest body

--- views/default/page/layouts/digest.php
+++ views/default/page/layouts/digest.php
@@ -5,10 +5,11 @@
 		// cache digest css as it is the same for all users / groups
 		$digest_css_contents = elgg_view("css/digest/core");
 	}
-
+	$language = get_current_language();
+	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<base target="_blank" />
