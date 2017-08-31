81f217c614c8ffc71b212f19604e6a7187e3f58e
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-08-31 13:58:34 +0200

    chore: changed double quotes to single quotes

--- start.php
+++ start.php
@@ -3,17 +3,17 @@
  * the main plugin file
  */
 
-define("DIGEST_INTERVAL_NONE", "none");
-define("DIGEST_INTERVAL_DEFAULT", "default");
-define("DIGEST_INTERVAL_DAILY", "daily");
-define("DIGEST_INTERVAL_WEEKLY", "weekly");
-define("DIGEST_INTERVAL_FORTNIGHTLY", "fortnightly");
-define("DIGEST_INTERVAL_MONTHLY", "monthly");
+define('DIGEST_INTERVAL_NONE', 'none');
+define('DIGEST_INTERVAL_DEFAULT', 'default');
+define('DIGEST_INTERVAL_DAILY', 'daily');
+define('DIGEST_INTERVAL_WEEKLY', 'weekly');
+define('DIGEST_INTERVAL_FORTNIGHTLY', 'fortnightly');
+define('DIGEST_INTERVAL_MONTHLY', 'monthly');
 
-require_once(dirname(__FILE__) . "/lib/functions.php");
+require_once(dirname(__FILE__) . '/lib/functions.php');
 
 // register elgg events
-elgg_register_event_handler("init", "system", "digest_init");
+elgg_register_event_handler('init', 'system', 'digest_init');
 
 /**
  * gets called when the system initializes
@@ -22,17 +22,17 @@ elgg_register_event_handler("init", "system", "digest_init");
  */
 function digest_init() {
 	// extend css
-	elgg_extend_view("css/elgg", "css/digest/site");
-	elgg_extend_view("css/admin", "css/digest/admin");
+	elgg_extend_view('css/elgg', 'css/digest/site');
+	elgg_extend_view('css/admin', 'css/digest/admin');
 	
 	// register page handler for nice url's
-	elgg_register_page_handler("digest", "digest_page_handler");
+	elgg_register_page_handler('digest', 'digest_page_handler');
 	
 	// extend register with subscribe option
-	elgg_extend_view("register/extend", "digest/register");
+	elgg_extend_view('register/extend', 'digest/register');
 	
 	// extend groups edit screen
-	elgg_extend_view("groups/edit", "digest/groupsettings/form", 400);
+	elgg_extend_view('groups/edit', 'digest/groupsettings/form', 400);
 	
 	// register plugin hooks
 	elgg_register_plugin_hook_handler('register', 'menu:page', '\ColdTrick\Digest\Menus::registerPageMenuItems');
@@ -49,12 +49,11 @@ function digest_init() {
 	elgg_register_event_handler('leave', 'group', '\ColdTrick\Digest\Groups::removeDigestSettingOnLeave');
 	
 	// register actions
-	elgg_register_action("digest/settings/save", dirname(__FILE__) . "/actions/settings/save.php", "admin");
-	elgg_register_action("digest/reset_stats", dirname(__FILE__) . "/actions/reset_stats.php", "admin");
+	elgg_register_action('digest/settings/save', dirname(__FILE__) . '/actions/settings/save.php', 'admin');
+	elgg_register_action('digest/reset_stats', dirname(__FILE__) . '/actions/reset_stats.php', 'admin');
 	
-	elgg_register_action("digest/update/usersettings", dirname(__FILE__) . "/actions/update/usersettings.php");
-	elgg_register_action("digest/update/groupsettings", dirname(__FILE__) . "/actions/update/groupsettings.php");
-	
+	elgg_register_action('digest/update/usersettings', dirname(__FILE__) . '/actions/update/usersettings.php');
+	elgg_register_action('digest/update/groupsettings', dirname(__FILE__) . '/actions/update/groupsettings.php');
 }
 
 /**
