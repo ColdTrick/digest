624670c8217b77efffa272557d0c9efe29ba101c
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-08-31 16:35:50 +0200

    chore: use elgg view for html elements

--- views/default/page/layouts/digest.php
+++ views/default/page/layouts/digest.php
@@ -3,41 +3,44 @@
 global $digest_css_contents;
 if (!isset($digest_css_contents)) {
 	// cache digest css as it is the same for all users / groups
-	$digest_css_contents = elgg_view("css/digest/core");
+	$digest_css_contents = elgg_view('css/digest/core');
 }
-$language = get_current_language();
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<base target="_blank" />
-		
-		<title><?php echo $vars["title"]; ?></title>
-	</head>
-	<body>
-		<style type="text/css">
-			<?php echo $digest_css_contents; ?>
-		</style>
-		<div id="digest_online">
-			<?php echo $vars["digest_online"]; ?>
+$title = elgg_extract('title', $vars);
+
+$head = <<<__HEAD
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<base target="_blank" />
+<title>$title</title>
+__HEAD;
+
+$body = <<<__BODY
+<style type="text/css">
+	$digest_css_contents
+</style>
+
+<div id="digest_online">
+	{$vars["digest_online"]}
+</div>
+<div id="digest_container">
+	<div>
+		<div id="digest_header">
+			{$vars["digest_header"]}
 		</div>
-		<div id="digest_container">
-			<div>
-				<div id="digest_header">
-					<?php echo $vars["digest_header"]; ?>
-				</div>
-				<div id="digest_content">
-					<?php echo $vars["content"]; ?>
-				</div>
-			</div>
-			<div id="digest_footer">
-				<?php echo $vars["footer"]; ?>
-			</div>
-			<div id="digest_unsubscribe">
-				<?php echo $vars["digest_unsubscribe"]; ?>
-			</div>
+		<div id="digest_content">
+			{$vars["content"]}
 		</div>
-	</body>
-</html>
+	</div>
+	<div id="digest_footer">
+		{$vars["footer"]}
+	</div>
+	<div id="digest_unsubscribe">
+		{$vars["digest_unsubscribe"]}
+	</div>
+</div>
+__BODY;
+
+echo elgg_view('page/elements/html', [
+	'head' => $head,
+	'body' => $body,
+]);
