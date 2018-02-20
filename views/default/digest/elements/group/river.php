3f057dd30886b0623e6b43324f98c4188feec10e
HEBUS\Seg <olivier.seguin@applisite.fr>
2018-02-20 18:48:50 +0100

    Fix missing comment river items (#37)

--- views/default/digest/elements/group/river.php
+++ views/default/digest/elements/group/river.php
@@ -14,17 +14,23 @@ $group_guid = $group->getGUID();
 $offset = 0;
 $limit = 25;
 
-// retrieve recent group activity
-$sql = "SELECT r.*";
-$sql .= " FROM " . $dbprefix . "river r";
-$sql .= " INNER JOIN " . $dbprefix . "entities AS e ON r.object_guid = e.guid"; // river event -> object
-$sql .= " WHERE (e.container_guid = $group_guid OR r.object_guid = $group_guid)"; // filter by group
-$sql .= " AND r.posted BETWEEN " . $ts_lower . " AND " . $ts_upper; // filter interval
-$sql .= " AND " . _elgg_get_access_where_sql(["table_alias" => "e"]); // filter access
-$sql .= " ORDER BY posted DESC";
-$sql .= " LIMIT " . $offset . "," . $limit;
+// set river options
+$options = [
+        'pagination' => false,
+	'limit' => $limit,
+	'offset' => $offset,
+	'distinct' => false,
+	'joins' => [
+		"JOIN {$dbprefix}entities e1 ON e1.guid = rv.object_guid",
+		"LEFT JOIN {$dbprefix}entities e2 ON e2.guid = rv.target_guid",
+	],
+	'wheres' => [
+		"(e1.container_guid = {$group_guid} OR e1.guid = {$group_guid} OR e2.container_guid = {$group_guid})",
+                "rv.posted BETWEEN " . $ts_lower . " AND " . $ts_upper
+	],
+];
 
-$items = get_data($sql, '_elgg_row_to_elgg_river_item');
+$items = elgg_get_river($options);
 
 if (empty($items)) {
 	return;
